28c5f1cc7dda546d4049f79559ddcd108ae35401
zyqaq412 <2791517764@qq.com>
2023-09-06 18:47:58 +0800

    first commit

---
+++ leadnews/leadnews-common/src/main/java/com/hzy/common/exception/CustomException.java
@@ -0,0 +1,16 @@
+package com.hzy.common.exception;
+
+import com.hzy.model.common.enums.AppHttpCodeEnum;
+
+public class CustomException extends RuntimeException {
+
+    private AppHttpCodeEnum appHttpCodeEnum;
+
+    public CustomException(AppHttpCodeEnum appHttpCodeEnum){
+        this.appHttpCodeEnum = appHttpCodeEnum;
+    }
+
+    public AppHttpCodeEnum getAppHttpCodeEnum() {
+        return appHttpCodeEnum;
+    }
+}
