a7c875a91a45c2e06e9627c7c9788ebee73a2d02
zyqaq412 <2791517764@qq.com>
2023-09-08 20:39:43 +0800

    文章详情-使用freemarker根据内容生成html文件上传到minio，访问文章详情时返回html文件路径

---
+++ leadnews/leadnews-service/leadnews-article/src/main/java/com/hzy/article/mapper/ApArticleContentMapper.java
@@ -0,0 +1,15 @@
+package com.hzy.article.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hzy.model.article.pojos.ApArticleContent;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @title: ApArticleContentMapper
+ * @Author zxwyhzy
+ * @Date: 2023/9/8 20:10
+ * @Version 1.0
+ */
+@Mapper
+public interface ApArticleContentMapper extends BaseMapper<ApArticleContent> {
+}
--- leadnews/leadnews-service/leadnews-article/src/main/resources/templates/article.ftl
+++ leadnews/leadnews-service/leadnews-article/src/main/resources/templates/article.ftl
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover">
-    <title>黑马头条</title>
+    <title>头条新闻</title>
     <!-- 引入样式文件 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vant@2.12.20/lib/index.css">
     <!-- 页面样式 -->
