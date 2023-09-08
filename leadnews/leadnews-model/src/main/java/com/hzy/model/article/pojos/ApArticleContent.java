013e426659e267cdcb046d9e6877ca1bebc7e838
zyqaq412 <2791517764@qq.com>
2023-09-08 16:02:13 +0800

    app文章列表-添加文章相关实体类

---
+++ leadnews/leadnews-model/src/main/java/com/hzy/model/article/pojos/ApArticleContent.java
@@ -0,0 +1,34 @@
+package com.hzy.model.article.pojos;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @title: ap_article_content APP已发布文章配置表
+ * @Author zxwyhzy
+ * @Date: 2023/9/8 15:59
+ * @Version 1.0
+ */
+@Data
+@TableName("ap_article_content")
+public class ApArticleContent implements Serializable {
+
+    @TableId(value = "id",type = IdType.ID_WORKER)
+    private Long id;
+
+    /**
+     * 文章id
+     */
+    @TableField("article_id")
+    private Long articleId;
+
+    /**
+     * 文章内容
+     */
+    private String content;
+}
